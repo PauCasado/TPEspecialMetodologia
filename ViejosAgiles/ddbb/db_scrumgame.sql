ae43b1e4a9db33ae7108aac0b4dcfd998b3286ad
Paula Casado <palita1416@gmail.com>
2020-10-09 19:08:01 -0300

    cambio de bbdd error en la anterior

--- ViejosAgiles/ddbb/db_scrumgame.sql
+++ ViejosAgiles/ddbb/db_scrumgame.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-10-2020 a las 16:26:49
+-- Tiempo de generación: 10-10-2020 a las 00:04:05
 -- Versión del servidor: 10.1.26-MariaDB
 -- Versión de PHP: 7.1.8
 
